8cbc2bb13fd2fe615e5626ca3ade1d13a5a2b242
Yuichiro Takeda <y-takeda.6147@i.softbank.jp>
2016-10-20 20:14:02 +0900

    Initial Commit

---
+++ PuzzDraRssReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PuzzDraRssReader
+//
+//  Created by 武田祐一郎 on 2016/10/20.
+//  Copyright © 2016年 Yuichiro-Takeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
